f28e3ed7c598dde78a394782a20814e3956faea8
Abhay <abhayjhalani25@gmail.com>
2023-06-13 09:00:02 +0530

    LeetCode 704

---
+++ BinarySearch.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int start = 0;
+        int end = n-1;
+
+        while(start<=end){
+            int mid = (start+end)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] < target){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
