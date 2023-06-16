25b27b6409643f87aacc625243bbf6b156f1886b
Abhay <abhayjhalani25@gmail.com>
2023-06-16 21:01:15 +0530

    LeetCode 509

---
+++ FibonacciNumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+       vector<int> f(35, 0);
+       f[0] = 0;
+       f[1] = 1;
+
+       for(int i = 2; i <= n; i++){
+           f[i] = f[i-1] + f[i-2];
+       }
+       return f[n];
+    }
+};
