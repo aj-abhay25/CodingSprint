55b9a2d4a9a5c0e856f466426989dbb463feee85
Abhay <abhayjhalani25@gmail.com>
2023-07-24 21:38:05 +0530

    LeetCode 1089

---
+++ Duplicate_Zeros.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        
+        for(int i = n-1; i >= 0; i--){
+            if(arr[i] == 0){
+                arr.insert(arr.begin() + i, 0);
+            }
+        }        
+        arr.resize(n);
+    }
+};
